37a16531a10573712b95b7842918d5b8216a3850
Hikiru <hikiruu@proton.me>
2025-10-25 01:39:12 -0500

    steam

--- hosts/anura/configuration.nix
+++ hosts/anura/configuration.nix
@@ -8,10 +8,20 @@
     ./hardware-configuration.nix
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   fileSystems = {
     "/".options = [ "compress=zstd" ];
     "/home".options = [ "compress=zstd" ];
-    "/nix".options = [ "compress=zstd" "noatime" ];
+    "/nix".options = [
+      "compress=zstd"
+      "noatime"
+    ];
   };
 
   networking.hostName = "anura";
