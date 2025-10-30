637bba207193ff612c21224fee6c443515f8adb5
Hikiru <hikiruu@proton.me>
2025-10-30 00:57:11 -0500

    proton ge

--- hosts/anura/configuration.nix
+++ hosts/anura/configuration.nix
@@ -14,6 +14,9 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     gamescopeSession.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 
   fileSystems = {
