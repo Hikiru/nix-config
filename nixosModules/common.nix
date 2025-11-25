d22dde720bd85d2303b2e98158a276a91f3271aa
Hikiru <hikiruu@proton.me>
2025-11-25 13:11:49 -0600

    japanese font

--- nixosModules/common.nix
+++ nixosModules/common.nix
@@ -100,6 +100,8 @@
   fonts = {
     packages = with pkgs; [
       noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
       noto-fonts-color-emoji
       nerd-fonts.caskaydia-cove
       dejavu_fonts
