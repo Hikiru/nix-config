6e789160249ca9aa0e030f03c874863aa5c2525c
Hikiru <hikiruu@proton.me>
2025-11-29 20:34:31 -0600

    create init.sh

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+HOST=$1
+
+if [[ -z $HOST ]]; then
+    echo "Hostname not provided."
+    exit 1
+fi
+
+echo "Making host \"$HOST\" directory..."
+mkdir -p hosts/$HOST
+
+echo "Copying hardware-configuration.nix..."
+cp -f /etc/nixos/hardware-configuration.nix hosts/$HOST
+
+echo "Activating configuration..."
+sudo nixos-rebuild switch --flake .#$HOST
+
+echo "Setting up chezmoi..."
+chezmoi init git@github.com:Hikiru/dotfiles.git
+chezmoi apply
